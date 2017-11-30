fb7435a125363963a4d7bf32b55e4933984fc34c
SC\keegan.lamparek <keeganlamparek@gmail.com>
2017-11-30 08:52:03 -0600

    Initial push.

---
+++ build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:53,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to Marvel Heroes and Villains\u2019s documentation!"],titleterms:{document:0,hero:0,indic:0,marvel:0,tabl:0,villain:0,welcom:0}})
